382c9c2f52a36bfb8f9141d65fd37b11697b9b95
lairaalmas <lairaalmas@gmail.com>
2023-02-22 16:56:52 -0300

    adding functions to context

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
+import AuthContext from "../../store/auth-context";
 import Navigation from "./Navigation";
 import classes from "./MainHeader.module.css";
 
 const MainHeader = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
     <header className={classes["main-header"]}>
       <h1>A Typical Page</h1>
-      <Navigation onLogout={props.onLogout} />
+      <Navigation />
     </header>
   );
 };
